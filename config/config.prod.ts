e2301b781f4582645355d36f2f1dcc5803e564ec
lynn <1057516363@qq.com>
2021-04-01 15:19:39 +0800

    fix: 测试代理

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -30,7 +30,7 @@ export default (appInfo: EggAppInfo) => {
   }
 
   config.security = {
-    domainWhiteList: [ 'http://owl-web.lynn.cool', 'http://172.17.0.5' ],
+    domainWhiteList: [ 'http://owl-web.lynn.cool', 'http://172.17.0.5:80' ],
   }
 
   config.logger = {

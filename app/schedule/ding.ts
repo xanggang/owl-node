de254a50227ba300e0e2b057f60ce58abf1d4abd
nx <1057516363@qq.com>
2021-12-02 16:48:08 +0800

    关闭定时器

--- app/schedule/ding.ts
+++ app/schedule/ding.ts
@@ -16,6 +16,7 @@ class UpdateCache extends Subscription {
     return {
       cron: '0 0 18 * * *', // 每天18点执行
       type: 'worker', // 每台机器上只有一个 worker 会执行这个定时任务，每次执行定时任务的 worker 的选择是随机的。
+      disable: true
     }
   }
 
